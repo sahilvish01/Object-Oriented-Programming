0017a56938ac3107c0478f3816fcaed8b82d8669
sahilvish01 <sahilvish6542@gmail.com>
2026-04-03 23:21:25 +0530

    Lower Right Pyramid

---
+++ 11.Patterns/c5.cpp
@@ -0,0 +1,24 @@
+// Lower Right Pyramid
+// * * *
+//   * *
+//     *
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            cout << "  ";
+        }
+        for(int j = 2; j >= i; j--)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
